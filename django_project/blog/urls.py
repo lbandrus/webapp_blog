24028cd5e5f8fc402a4d82f1ceffe99732368853
lbandrus <loganbandrus@gmail.com>
2022-07-04 00:21:27 -0600

    finished webapp

---
+++ django_project/blog/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from .views import PostListView, PostDetailView, PostcreateView, PostUpdateView
+from . import views
+
+urlpatterns = [
+    path('', PostListView.as_view(), name='blog-home'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('post/new/', PostcreateView.as_view(), name='post-create'),
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
+    path('about/', views.about, name='blog-about'),
+]
